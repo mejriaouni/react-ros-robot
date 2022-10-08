037acf8fd9feb779175be13d15ec1846fab02570
mejriaouni <mejriaouni@gmail.com>
2022-10-08 12:57:15 +0100

    section 3 working

--- src/components/Connection.jsx
+++ src/components/Connection.jsx
@@ -1,5 +1,7 @@
-import React , {Component} from "react";
+import React, { Component } from "react";
 import Alert from "react-bootstrap/Alert";
+import Config from "../scripts/config";
+
 
 
 
@@ -7,15 +9,53 @@ import Alert from "react-bootstrap/Alert";
 
 
 class Connection extends Component {
-    state = { connected : false}
-    init_connection()
-    {
-       var ros = new window.ROSLIB.Ros();
+    state = { connected: false, ros: null };
+    constructor() {
+        super();
+        this.init_connection();
     }
-    render(){
+    init_connection() {
+        this.state.ros = new window.ROSLIB.Ros();
+        console.log(this.state.ros);
+
+        this.state.ros.on("connection", () => {
+            console.log("connection established! ");
+            this.setState({ connected: true });
+        });
+
+        this.state.ros.on("close", () => {
+            console.log("connection is closed ");
+            this.setState({ connected: false });
+            console.log(Config.ROSBRIDGE_SERVER_IP );
+
+            //try to reconnect after a fixed period of time 
+            setTimeout(() => {
+                try {
+                  
+           //this.state.ros.connect("ws://192.168.1.46:9090");
+            this.state.ros.connect("ws://" + Config.ROSBRIDGE_SERVER_IP + ":" + Config.ROSBRIDGE_SERVER_PORT);
+                } catch (error) {
+                    console.log("connection problem! ");
+
+
+                }
+            }, Config.RECONNECTION_TIMER);
+        });
+        try {
+          //  this.state.ros.connect("ws://192.168.1.46:9090");
+            this.state.ros.connect("ws://" + Config.ROSBRIDGE_SERVER_IP + ":" + Config.ROSBRIDGE_SERVER_PORT);
+
+        } catch (error) {
+            console.log("connection problem! ");
+
+
+        }
+
+    }
+    render() {
         return (
             <div>
-                <Alert className="text-center m-3" variant={this.state.connected? "success" : "danger"}>{this.state.connected? "Robot Connected" : "Robot Disconnected"}</Alert>
+                <Alert className="text-center m-3" variant={this.state.connected ? "success" : "danger"}>{this.state.connected ? "Robot Connected" : "Robot Disconnected"}</Alert>
             </div>
         );
     }
---
+++ src/scripts/config.js
@@ -0,0 +1,6 @@
+const Config  = {
+    ROSBRIDGE_SERVER_IP: "192.168.1.46",
+    ROSBRIDGE_SERVER_PORT: "9090",
+    RECONNECTION_TIMER:10000,
+}
+export default Config
